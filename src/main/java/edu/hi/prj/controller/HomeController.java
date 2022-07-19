2a808fc6ba93d36732b9d0af8107c0d54b5b862e
TEACHER\PC-00 <nqwrt@naver.com>
2022-07-19 15:16:18 +0900

    first commit!! 아자아자 파이팅!!

---
+++ src/main/java/edu/hi/prj/controller/HomeController.java
@@ -0,0 +1,15 @@
+package edu.hi.prj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello 프로젝트명";
+		
+	}
+
+}
